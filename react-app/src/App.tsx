51d8252e19a090166fc04f99248b75effbd18c61
RITESH BANSAL <riteshbansal3@gmail.com>
2026-05-05 18:31:08 -0700

    react-course1

---
+++ react-app/src/App.tsx
@@ -0,0 +1,19 @@
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="warning" onClick={() => setAlertVisibility(true)}>
+        My Button{" "}
+      </Button>
+    </div>
+  );
+}
+export default App;
---
+++ react-app/src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React, { type ReactNode } from "react";
+interface Props {
+  children: ReactNode;
+  color?: "primary" | "secondary" | "danger" | "warning";
+  onClick: () => void;
+}
+
+const Button = ({ children, color = "primary", onClick }: Props) => {
+  return (
+    <button className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
